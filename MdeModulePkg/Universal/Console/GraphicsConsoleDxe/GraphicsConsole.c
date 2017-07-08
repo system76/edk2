20a7334a96686a5309af7582274328f0add108a3
Matt DeVillier <matt.devillier@gmail.com>
2017-07-07 20:35:11 -0500

    MdeModulePkg/GraphicsConsole: don't draw cursor at 0,0
    Prevents cursor from flashing on screen when
    changing modes or clearing the screen.
    
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- MdeModulePkg/Universal/Console/GraphicsConsoleDxe/GraphicsConsole.c
+++ MdeModulePkg/Universal/Console/GraphicsConsoleDxe/GraphicsConsole.c
@@ -1943,7 +1943,8 @@ FlushCursor (
 
   CurrentMode = This->Mode;
 
-  if (!CurrentMode->CursorVisible) {
+  if (!CurrentMode->CursorVisible ||
+      (CurrentMode->CursorColumn == 0 && CurrentMode->CursorRow == 0 )) {
     return EFI_SUCCESS;
   }
 
