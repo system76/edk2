04b1f303f1b272b985c96de38893472e7df3e657
Tim Crawford <tcrawford@system76.com>
2020-01-31 11:27:44 -0700

    UefiPayloadPkg: Disable EFI shell

--- UefiPayloadPkg/Library/PlatformBootManagerLib/PlatformBootManager.c
+++ UefiPayloadPkg/Library/PlatformBootManagerLib/PlatformBootManager.c
@@ -238,7 +238,7 @@ PlatformBootManagerAfterConsole (
   //
   // Register UEFI Shell
   //
-  PlatformRegisterFvBootOption (PcdGetPtr (PcdShellFile), L"UEFI Shell", LOAD_OPTION_ACTIVE);
+  //PlatformRegisterFvBootOption (PcdGetPtr (PcdShellFile), L"UEFI Shell", LOAD_OPTION_ACTIVE);
 
   // Show prompt at bottom center
   BootLogoUpdateProgress (
