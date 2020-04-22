b376a7d60f0fc000582c71a8fa6d1e0240b5421f
Matt DeVillier <matt.devillier@gmail.com>
2020-04-22 17:54:38 -0500

    ShellPkg: disable startup script, show welcome banner
    Startup script does nothing other than confuse users.
    Show a welcome banner and tell users how to exit.
    
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- ShellPkg/Application/Shell/Shell.c
+++ ShellPkg/Application/Shell/Shell.c
@@ -587,11 +587,16 @@ UefiMain (
         ConInHandle   = NULL;
       }
 
-      if (!EFI_ERROR(Status) && PcdGet8(PcdShellSupportLevel) >= 1) {
+      if (FALSE && !EFI_ERROR(Status) && PcdGet8(PcdShellSupportLevel) >= 1) {
         //
         // process the startup script or launch the called app.
         //
         Status = DoStartupScript(ShellInfoObject.ImageDevPath, ShellInfoObject.FileDevPath);
+      } else {
+        //
+        // show welcome notice
+        //
+        ShellPrintHiiEx(-1, -1, NULL, STRING_TOKEN (STR_SHELL_WELCOME), ShellInfoObject.HiiHandle);
       }
 
       if (!ShellInfoObject.ShellInitSettings.BitUnion.Bits.Exit && !ShellCommandGetExit() && (PcdGet8(PcdShellSupportLevel) >= 3 || PcdGetBool(PcdShellForceConsole)) && !EFI_ERROR(Status) && !ShellInfoObject.ShellInitSettings.BitUnion.Bits.NoConsoleIn) {
