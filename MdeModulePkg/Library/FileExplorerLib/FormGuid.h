5b25fe5b8474fd8388ba45ac36281c116dbe75bb
Tim Crawford <tcrawford@system76.com>
2020-06-25 23:30:41 -0600

    MdeModulePkg/BMM: Limit boot options to change order and file explorer

--- MdeModulePkg/Library/FileExplorerLib/FormGuid.h
+++ MdeModulePkg/Library/FileExplorerLib/FormGuid.h
@@ -16,7 +16,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
   0xfe561596, 0xe6bf, 0x41a6, {0x83, 0x76, 0xc7, 0x2b, 0x71, 0x98, 0x74, 0xd0} \
   }
 
-#define FORM_FILE_EXPLORER_ID                0x1000
+#define FORM_FILE_EXPLORER_ID                0x1001
 #define FORM_ADD_NEW_FILE_ID                 0x2000
 #define NEW_FILE_NAME_ID                     0x2001
 #define KEY_VALUE_CREATE_FILE_AND_EXIT       0x2002
