542d2ad72ef279258a32708e02eb1f84cb6d5f76
Patrick Rudolph <patrick.rudolph@9elements.com>
2020-10-12 07:55:44 -0600

    UefipayloadPkg: Protect coreboot tables
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- UefiPayloadPkg/UefiPayloadEntry/UefiPayloadEntry.c
+++ UefiPayloadPkg/UefiPayloadEntry/UefiPayloadEntry.c
@@ -236,10 +236,24 @@ BuildHobFromBl (
   EFI_PEI_GRAPHICS_DEVICE_INFO_HOB *NewGfxDeviceInfo;
 
   //
-  // Report lower 640KB of RAM. Attribute EFI_RESOURCE_ATTRIBUTE_TESTED
-  // is intentionally omitted to prevent erasing of the coreboot header
-  // record before it is processed by ParseMemoryInfo.
+  // Report lower 640KB of RAM.
+  // Mark memory as reserved to keep coreboot header in place.
   //
+  BuildResourceDescriptorHob (
+    EFI_RESOURCE_MEMORY_RESERVED,
+    (
+    EFI_RESOURCE_ATTRIBUTE_PRESENT |
+    EFI_RESOURCE_ATTRIBUTE_INITIALIZED |
+    EFI_RESOURCE_ATTRIBUTE_TESTED |
+    EFI_RESOURCE_ATTRIBUTE_UNCACHEABLE |
+    EFI_RESOURCE_ATTRIBUTE_WRITE_COMBINEABLE |
+    EFI_RESOURCE_ATTRIBUTE_WRITE_THROUGH_CACHEABLE |
+    EFI_RESOURCE_ATTRIBUTE_WRITE_BACK_CACHEABLE
+    ),
+    (EFI_PHYSICAL_ADDRESS)(0),
+    (UINT64)(0x1000)
+    );
+
   BuildResourceDescriptorHob (
     EFI_RESOURCE_SYSTEM_MEMORY,
     (
@@ -250,8 +264,8 @@ BuildHobFromBl (
     EFI_RESOURCE_ATTRIBUTE_WRITE_THROUGH_CACHEABLE |
     EFI_RESOURCE_ATTRIBUTE_WRITE_BACK_CACHEABLE
     ),
-    (EFI_PHYSICAL_ADDRESS)(0 + 0x1000),
-    (UINT64)(0xA0000 - 0x1000)
+    (EFI_PHYSICAL_ADDRESS)(0x1000),
+    (UINT64)(0x9F000)
     );
 
   BuildResourceDescriptorHob (
