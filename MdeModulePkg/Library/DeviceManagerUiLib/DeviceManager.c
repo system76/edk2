e450bcfb721f9664c71820d1b36b2a4f8e1d9076
Tim Crawford <tcrawford@system76.com>
2026-08-26 16:41:12 -0600

    Revert "DeviceManager: Add empty lines between entries"
    This reverts commit 55d53ffcbf0acd0572fe8285ec89e1e53f7bda67.

--- MdeModulePkg/Library/DeviceManagerUiLib/DeviceManager.c
+++ MdeModulePkg/Library/DeviceManagerUiLib/DeviceManager.c
@@ -650,9 +650,6 @@ CreateDeviceManagerForm (
             //
             if (!AddNetworkMenu) {
               AddNetworkMenu = TRUE;
-              // Add an empty line before each entry
-              HiiCreateSubTitleOpCode (StartOpCodeHandle, STRING_TOKEN (STR_EMPTY_STRING), 0, 0, 0);
-
               HiiCreateGotoOpCode (
                 StartOpCodeHandle,
                 NETWORK_DEVICE_LIST_FORM_ID,
@@ -667,9 +664,6 @@ CreateDeviceManagerForm (
             // In network device list form, same mac address device only show one menu.
             //
             while (AddItemCount > 0) {
-              // Add an empty line before each entry
-              HiiCreateSubTitleOpCode (StartOpCodeHandle, STRING_TOKEN (STR_EMPTY_STRING), 0, 0, 0);
-
               HiiCreateGotoOpCode (
                 StartOpCodeHandle,
                 NETWORK_DEVICE_FORM_ID,
@@ -690,9 +684,6 @@ CreateDeviceManagerForm (
               DevicePathId =  HiiSetString (HiiHandle, 0, DevicePathStr, NULL);
               FreePool (DevicePathStr);
             }
-
-            // Add an empty line before each entry
-            HiiCreateSubTitleOpCode (StartOpCodeHandle, STRING_TOKEN (STR_EMPTY_STRING), 0, 0, 0);
 
             HiiCreateGotoExOpCode (
               StartOpCodeHandle,
@@ -717,9 +708,6 @@ CreateDeviceManagerForm (
               DevicePathId =  HiiSetString (HiiHandle, 0, DevicePathStr, NULL);
               FreePool (DevicePathStr);
             }
-
-            // Add an empty line before each entry
-            HiiCreateSubTitleOpCode (StartOpCodeHandle, STRING_TOKEN (STR_EMPTY_STRING), 0, 0, 0);
 
             HiiCreateGotoExOpCode (
               StartOpCodeHandle,
